5a21a79912b34c090307f4fc964aa82684f49c4c
christian-0s <de.santis.christian@gmail.com>
2026-03-25 14:26:42 +0100

    Update blink.c
    Fix error in previous PR

--- src/blink.c
+++ src/blink.c
@@ -13,8 +13,6 @@ void blink_init(void) {
   gpio_put(PICO_DEFAULT_WS2812_POWER_PIN, true);
 #endif
 #ifdef PICO_DEFAULT_WS2812_PIN
-#define WS2812_PIN PICO_DEFAULT_WS2812_PIN
-#else
 // default to pin 2 if the board doesn't have a default WS2812 pin defined
 #define PICO_DEFAULT_WS2812_PIN 2
 #endif
